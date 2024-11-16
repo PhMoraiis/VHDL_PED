cb8590d1a0a27ad0b600153c2696621e998ea4b5
Philipe Morais <liperapltda@gmail.com>
2024-11-16 13:26:45 -0300

    first commit

---
+++ docs/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+import nextra from 'nextra'
+ 
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+
+const nextConfig: NextConfig = {
+  basePath: '/docs',
+  output: "export",
+  images: {
+    unoptimized: true
+  }
+  /* config options here */
+};
+
+export default withNextra(nextConfig)
