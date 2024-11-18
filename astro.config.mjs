2f045a841f4f4f77488f5d7b064e54bb378a5ebf
Philipe Morais <liperapltda@gmail.com>
2024-11-18 16:40:54 -0300

    test starlight2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,40 +6,55 @@ import starlightImageZoomPlugin from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "phmoraiis.github.io/docs_testing",
+	base: '/',
+	srcDir: './src',
 	integrations: [
 		starlight({
 			plugins: [starlightImageZoomPlugin()],
-			title: 'Submin',
+			title: "Submin",
 			logo: {
 				replacesTitle: true,
-				dark: './src/assets/dark.svg',
-				light: './src/assets/light.svg',
+				dark: "./src/assets/dark.svg",
+				light: "./src/assets/light.svg",
 			},
 			social: {
-				github: 'https://github.com/mdsreq-fga-unb/2024.2-T03-Submin',
+				github: "https://github.com/mdsreq-fga-unb/2024.2-T03-Submin",
 			},
 			sidebar: [
 				{
-					label: 'Introdução',
+					label: "Introdução",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Bem-vindo', slug: 'intro/introduction' },
+						{ label: "Bem-vindo", slug: "intro/introduction" },
 					],
 				},
 				{
-					label: 'Visão de Produto e Projeto',
+					label: "Visão de Produto e Projeto",
 					items: [
-						{ label: 'Cenário Atual do Cliente e do Negócio', slug: 'unidade-1/cenario'},
-						{ label: 'Soluções Propostas', slug: 'unidade-1/solucao'},
-						{ label: 'Estratégias de Engenharia de Software', slug: 'unidade-1/estrategias'},
-						{ label: 'Cronograma e Entregas', slug: 'unidade-1/cronograma'},
-						{ label: 'Interação Entre Equipe e Cliente', slug: 'unidade-1/interacao'},
-						{ label: 'Lições Aprendidas', slug: 'unidade-1/licoes'},
-						{ label: 'Referências Bibliográficas', slug: 'unidade-1/referencias'},
+						{
+							label: "Cenário Atual do Cliente e do Negócio",
+							slug: "unidade-1/cenario",
+						},
+						{ label: "Soluções Propostas", slug: "unidade-1/solucao" },
+						{
+							label: "Estratégias de Engenharia de Software",
+							slug: "unidade-1/estrategias",
+						},
+						{ label: "Cronograma e Entregas", slug: "unidade-1/cronograma" },
+						{
+							label: "Interação Entre Equipe e Cliente",
+							slug: "unidade-1/interacao",
+						},
+						{ label: "Lições Aprendidas", slug: "unidade-1/licoes" },
+						{
+							label: "Referências Bibliográficas",
+							slug: "unidade-1/referencias",
+						},
 					],
 				},
 			],
-			customCss: ['./src/tailwind.css'],
+			customCss: ["./src/tailwind.css"],
 		}),
 		tailwind({ applyBaseStyles: false }),
 	],
